cd28850bb8e63d1fd3f68c2f1d9d1e29f981cdb2
OT <imot@protonmail.com>
2017-10-15 11:11:07 +0400

    Update vpnbook.sh

--- vpnbook.sh
+++ vpnbook.sh
@@ -15,7 +15,7 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 banner="\t\t\t${red}VPNBOOK${reset}${grey} - VPNBOOK Client v1.1.2${reset}\n ${ear}^${reset}${eye}>${reset}${mouth}_${reset}${eye}<${reset}${ear}^${reset}\t\t ${bold}${t}_${reset}\n\t${bold}${o}0${reset}${normal}${bold}${t}7${reset}${normal}\t${o}[${reset}${t}|${reset}${o}]${reset}${normal} ${bold}${o}0${reset}${normal}${otext}ffensive${reset} ${bold}${t}7${reset}${normal}${ttext}ester${reset}\n"
 echo -e ${banner}
-
+### Welcome text
 if [[ $EUID -ne 0 ]]; then
 	echo "This script must be run as root" 
 	exit 1
@@ -29,10 +29,8 @@ if ! [ -d /etc/openvpn ] && ! [ -f /usr/bin/curl ] && ! [ -f /usr/bin/grep ] && 
 	apt-get install sed -y
 	exit 1
 fi
-
-
+### Check addons
 	echo -e "vpnbook\n"$( echo $(echo $(curl -s https://www.vpnbook.com/ | echo $(grep "<li><strong>Password: " | head -1 )) | tr -d "<li><strong>Password: //"))"\n" > data.txt
 	sed -i '$ d' data.txt
 	openvpn vpnbook-de233-tcp80.ovpn &>/dev/null
-
 exit 0
