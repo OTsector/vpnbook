cbd6a65b124a4a9008256dfe849f23f5b9f8d11b
OT <imot@protonmail.com>
2020-04-29 05:07:53 -0400

    Add files via upload

--- vpnbook.sh
+++ vpnbook.sh
@@ -11,7 +11,7 @@ ttext=`tput setaf 61`
 reset=`tput sgr0`
 bold=$(tput bold)
 normal=$(tput sgr0)
-banner=" \t\t ${bold}${t}_${reset}\n\t${bold}${o}0${reset}${normal}${bold}${t}7${reset}${normal}\t${o}[${reset}${t}|${reset}${o}]${reset}${normal} ${bold}${o}0${reset}${normal}${otext}ffensive${reset} ${bold}${t}7${reset}${normal}${ttext}ester${reset}\n\n\t# ${red}VPNBOOK${reset}${grey} - VPNBOOK Client v1.5.2${reset}\n"
+banner=" \t\t ${bold}${t}_${reset}\n\t${bold}${o}0${reset}${normal}${bold}${t}7${reset}${normal}\t${o}[${reset}${t}|${reset}${o}]${reset}${normal} ${bold}${o}0${reset}${normal}${otext}ffensive${reset} ${bold}${t}7${reset}${normal}${ttext}ester${reset}\n\n\t# ${red}VPNBOOK${reset}${grey} - VPNBOOK Client v1.5.3${reset}\n"
 echo -e ${banner}
 ### Welcome text
 if [[ $EUID -ne 0 ]]; then
@@ -29,10 +29,16 @@ if ! [ -d /etc/openvpn ] && ! [ -f /usr/bin/curl ] && ! [ -f /usr/bin/grep ] && 
 fi
 ### Check addons
 	echo "Starting ${green}${bold}VPN${normal}${reset} service..."
-	wget https://www.vpnbook.com/free-openvpn-account/VPNBook.com-OpenVPN-Euro1.zip -q -O $PWD/temp/euro1.zip &>/dev/null
-	unzip -o $PWD/temp/euro1.zip -d $PWD/temp/ &>/dev/null
-	echo -e "vpnbook\n"$( echo $(echo $(curl -s https://www.vpnbook.com/ | echo $(grep "<li><strong>Password: " | head -1 )) | sed 's/<li><strong>Password: //g' | sed -e 's/<\/strong><\/li>//g'))"\n" > $PWD/temp/data.txt
+	wget https://www.vpnbook.com/free-openvpn-account/VPNBook.com-OpenVPN-FR8.zip -q -O $PWD/temp/fr.zip &>/dev/null
+	unzip -o $PWD/temp/fr.zip -d $PWD/temp/ &>/dev/null
+	while true; do
+		code="$(pc curl -sLgA '' 'https://www.vpnbook.com/password.php?t=0.85492300%201588143263' \
+			-o /tmp/.gregefrd.png&&gocr /tmp/.gregefrd.png;rm -rf /tmp/.gregefrd.png)"
+		if [ ${#code} -gt 0 ]; then break; fi
+	done
+	echo -e "vpnbook\n$code\n" > $PWD/temp/data.txt
 	sed -i '$ d' $PWD/temp/data.txt
 	echo -e "\nCTRL+C ${red}to exit${reset}"
 	openvpn --config $PWD/temp/*53.ovpn --auth-user-pass $PWD/temp/data.txt &>/dev/null
 exit 1 | clear && echo -e "${banner}\n${red}Disconnected${reset}"
+
